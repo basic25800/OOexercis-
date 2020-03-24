29690971368c5a0bee9e894075a8df46dbceba42
tmddu <tmddu@192.168.1.6>
2020-03-24 15:01:50 +0900

    sarch 작성
    update 작성

--- src/com/kh/member/controller/MemberManager.java
+++ src/com/kh/member/controller/MemberManager.java
@@ -33,6 +33,102 @@ public class MemberManager {
 		System.out.println("입력이 완료되었습니다. 메인메뉴로 돌아갑니다.");
 
 	}
+	public void searchId() {
+		System.out.print("검색할 아이디를 입력하세요 : ");
+		String id = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getUserId().equalsIgnoreCase(id)) {
+				printOne(marr[i]);
+				break;
+			} else {
+				System.out.println("삭제할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+	}
+	public void searchName() {
+		System.out.println("검새할 이름을 입력하세요 : ");
+		String name = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getUserName().equalsIgnoreCase(name)) {
+				printOne(marr[i]);
+				break;
+			} else {
+				System.out.println("검색할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+	}
+	public void searchEmail() {
+		System.out.println("검색할 이메일을 입력하세요 : ");
+		String email = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getEmail().equalsIgnoreCase(email)) {
+				printOne(marr[i]);
+				break;
+			} else {
+				System.out.println("검색한 회원 정보가 존재하지 않습니다. ");
+				break;
+			}
+		}
+	}
+	public void updatePwd() {
+		System.out.println("수정할 회원의 아이디를 입력하세요 : ");
+		String id = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getUserId().equalsIgnoreCase(id)) {
+				System.out.println("변경할 비밀번호 : ");
+				marr[i].setUserPwd(sc.nextLine());
+				break;
+			} else {
+				System.out.println("검색할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+	}
 
 
+	public void updateName() {
+		System.out.println("수정할 회원의 이름을 입력하세요 : ");
+		String name = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getUserName().equalsIgnoreCase(name)) {
+				System.out.println("변경할 이름 : ");
+				marr[i].setUserName(sc.nextLine());
+				break;
+			} else {
+				System.out.println("검색할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+			
+	}
+	public void updateEmail() {
+		System.out.println("수정할 회원의 이메일을 입력하세요 : ");
+		String email = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getEmail().equalsIgnoreCase(email)) {
+				System.out.println("변경할 이메일 : ");
+				marr[i].setEmail(sc.nextLine());
+				break;
+			}	else {
+				System.out.println("검색할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+	}
+	public void deleteOne() {
+		System.out.println("탈퇴할 회원의 아이디를 입력하세요 : ");
+		String id = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+			if(marr[i].getUserId().equalsIgnoreCase(id)) {
+				marr[i+1] = marr[i];
+				break;
+			} else {
+				System.out.println("삭제할 회원 정보가 존재하지 않습니다.");
+				break;
+			}
+		}
+		
+	}
 }
