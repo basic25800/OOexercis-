8119ae3d2c59702a887106633d4a1f5187795404
tmddu <tmddu@192.168.1.6>
2020-03-24 13:49:57 +0900

    setter 작성
    getter 작성

--- src/com/kh/member/model/vo/Member.java
+++ src/com/kh/member/model/vo/Member.java
@@ -18,4 +18,40 @@ public class Member {
 		this.gender = gender;
 		this.email = email;
 	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public String getEmail() {
+		return email;
+	}
 }
